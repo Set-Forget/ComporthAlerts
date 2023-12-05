25add673da560c3232ff06ba0d50607a9776edad
juan troncoso <troncosojuanbernabe@gmail.com>
2023-12-05 11:51:06 -0300

    Actualizar variables de entorno y configuraciones

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Login({
       options: {
         redirectTo: (() => {
           let url =
-          process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+          "https://set-forget.github.io/ComporthAlerts/auth/callback" ?? // Set this to your site URL in production env.
           process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
           "http://localhost:3000/auth/callback";
           // Make sure to include `https://` when not localhost.
